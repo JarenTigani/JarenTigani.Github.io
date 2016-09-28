8c94d5606d9c0ac2296b0ffd16b999e56b63a148
JarenTigani <jtigani@collegeforcreativestudies.edu>
2016-09-28 19:18:55 -0400

    updated

--- js/script.js
+++ js/script.js
@@ -36,26 +36,26 @@ function checkNumber(numberTime) {
 };
 
 function showDiurnal() {
-    if (timer.seconds >= 20 && 7 <= timer.seconds) {
-      $("#animal-2, #animal-4, #animal-6, #animal-8, #animal-10, #animal-12, #animal-16").fadeIn().show();
+    if (timer.seconds >= 11 && 40 <= timer.seconds) {
+      $("#animal-2, #animal-6, #animal-8, #animal-10, #animal-11, #animal-12, #animal-15, #animal-17, #animal-18, #animal-19, #animal-20, #animal-21, #animal-24, #animal-34, #animal-35, #animal-36, #animal-39, #animal-40, #animal-41, #animal-42").fadeIn().show();
     } else {
-      $("#animal-2, #animal-4, #animal-6, #animal-8, #animal-10, #animal-12, #animal-16").fadeOut().hide();
+      $("#animal-2, #animal-6, #animal-8, #animal-10, #animal-11, #animal-12, #animal-15, #animal-17, #animal-18, #animal-19, #animal-20, #animal-21, #animal-24, #animal-34, #animal-35, #animal-36, #animal-39, #animal-40, #animal-41, #animal-42").fadeOut().hide();
     }
   };
   
 function showNocturnal() {
-    if (timer.seconds >= 30 && 50 <= timer.seconds) {
-      $("#animal-1, #animal-3, #animal-5, #animal-7, #animal-9, #animal-11, #animal-13, #animal-15, #animal-17, #animal-19,#animal-21").fadeIn().show();
+    if (timer.seconds >= 41 && 59 <= timer.seconds) {
+      $("#animal-1, #animal-3, #animal-4, #animal-5, #animal-13, #animal-14, #animal-16, #animal-23, #animal-26, #animal-28, #animal-29, #animal-31, #animal-32, #animal-33, #animal-38, #animal-45"  ).fadeIn().show();
     } else {
-      $("#animal-1, #animal-3, #animal-5, #animal-7, #animal-9, #animal-11, #animal-13, #animal-15, #animal-17, #animal-19,#animal-21").fadeOut().hide();
+      $("#animal-1, #animal-3, #animal-4, #animal-5, #animal-13, #animal-14, #animal-16, #animal-23, #animal-26, #animal-28, #animal-29, #animal-31, #animal-32, #animal-33, #animal-38, #animal-45").fadeOut().hide();
     }
   };
   
   function showCrepuscular() {
     if (timer.seconds >= 0 && 10 <= timer.seconds) {
-      $("#animal-18, #animal-20, #animal-22, #animal-23, #animal-24, #animal-25, #animal-26").fadeIn().show();
+      $("#animal-7, #animal-9, #animal-22, #animal-27, #animal-30, #animal-37, #animal-43, #animal-44").fadeIn().show();
     } else {
-      $("#animal-18, #animal-20, #animal-22, #animal-23, #animal-24, #animal-25, #animal-26").fadeOut().hide();
+      $("#animal-7, #animal-9, #animal-22, #animal-27, #animal-30, #animal-37, #animal-43, #animal-44").fadeOut().hide();
     }
   };
 
