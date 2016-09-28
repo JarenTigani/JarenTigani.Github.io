0fe2e95de4dcfe32230e93d4872e0b9e78904a99
JarenTigani <jtigani@collegeforcreativestudies.edu>
2016-09-28 19:46:42 -0400

    change

--- js/script.js
+++ js/script.js
@@ -36,7 +36,7 @@ function checkNumber(numberTime) {
 };
 
 function showDiurnal() {
-    if (timer.seconds >= 11 && 40 <= timer.seconds) {
+    if (timer.seconds >= 15 && 30 <= timer.seconds) {
       $("#animal-2, #animal-6, #animal-8, #animal-10, #animal-11, #animal-12, #animal-15, #animal-17, #animal-18, #animal-19, #animal-20, #animal-21, #animal-24, #animal-34, #animal-35, #animal-36, #animal-39, #animal-40, #animal-41, #animal-42").fadeIn().show();
     } else {
       $("#animal-2, #animal-6, #animal-8, #animal-10, #animal-11, #animal-12, #animal-15, #animal-17, #animal-18, #animal-19, #animal-20, #animal-21, #animal-24, #animal-34, #animal-35, #animal-36, #animal-39, #animal-40, #animal-41, #animal-42").fadeOut().hide();
@@ -44,7 +44,7 @@ function showDiurnal() {
   };
   
 function showNocturnal() {
-    if (timer.seconds >= 41 && 59 <= timer.seconds) {
+    if (timer.seconds >= 31 && 59 <= timer.seconds) {
       $("#animal-1, #animal-3, #animal-4, #animal-5, #animal-13, #animal-14, #animal-16, #animal-23, #animal-26, #animal-28, #animal-29, #animal-31, #animal-32, #animal-33, #animal-38, #animal-45"  ).fadeIn().show();
     } else {
       $("#animal-1, #animal-3, #animal-4, #animal-5, #animal-13, #animal-14, #animal-16, #animal-23, #animal-26, #animal-28, #animal-29, #animal-31, #animal-32, #animal-33, #animal-38, #animal-45").fadeOut().hide();
@@ -52,7 +52,7 @@ function showNocturnal() {
   };
   
   function showCrepuscular() {
-    if (timer.seconds >= 0 && 10 <= timer.seconds) {
+    if (timer.seconds >= 1 && 14 <= timer.seconds) {
       $("#animal-7, #animal-9, #animal-22, #animal-27, #animal-30, #animal-37, #animal-43, #animal-44").fadeIn().show();
     } else {
       $("#animal-7, #animal-9, #animal-22, #animal-27, #animal-30, #animal-37, #animal-43, #animal-44").fadeOut().hide();
